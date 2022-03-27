4754459dd89fcef7849a383bab8a89ecfba966c1
rishabh <rishabh.dwivedi@noborderz.com>
2022-03-27 11:35:01 +0530

    arrow icon hide and resolved height issue

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -16,8 +16,8 @@ function LoginPage({ dispatch }) {
       <div className="inner-part-page auth-section">
         <div className="container">
           <div className="auth-section">
-            <div className="d-block d-md-none login-text">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
+            <div className="d-block d-md-none login-text login-text-height">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-left d-none"><polyline points="15 18 9 12 15 6"></polyline></svg>
               <span>
                 Login
                 <img src="/images/line.png" alt="line" />
