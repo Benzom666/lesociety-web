675fd2f2c8c0c9eda65313f1eaec7fee7e55400b
Rishabh <rishabh.dwivedi@noborderz.com>
2022-05-12 22:02:44 +0530

    Changes: API Integration on Post date oage

--- modules/date/datePreview.js
+++ modules/date/datePreview.js
@@ -8,6 +8,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router';
 import UserCardDetail from '@/core/UserCardDetail';
 import ConfirmDate from './../../modules/date/confirmDate';
+import { apiRequest } from 'utils/Utilities';
 
 const DatePreview = props => {
     const { handleSubmit, previousPage, invalid, pristine, submitting, onClose } = props
@@ -21,16 +22,32 @@ const DatePreview = props => {
     const timeState = useSelector(state => state?.form?.CreateStepThree?.values);
     const dateDescription = useSelector(state => state?.form?.CreateStepFour?.values)
     const [confirmPopup, setConfirmPopup] = useState(false);
+    const [loader, setLoader] = useState(false);
 
     const toggle = () => setConfirmPopup(!confirmPopup);
 
-    const postDate = () => {
-        router.push({ pathname: '/user/user-list', query: { city: cityState?.enter_city?.name, country: cityState.enter_country?.value, province: cityState?.enter_city?.province[0]?.short_code?.split("-")[1] ? cityState?.enter_city?.province[0]?.short_code?.split("-")[1] : cityState?.enter_city?.province[0]?.short_code } }, '/user/user-list');
-        dispatch(reset('ChooseCity'))
-        dispatch(reset('CreateStepOne'))
-        dispatch(reset('CreateStepTwo'))
-        dispatch(reset('CreateStepThree'))
-        dispatch(reset('CreateStepFour'))
+    const postDate = async () => {
+        setLoader(true)
+        const data = {
+            date_status: true,
+        }
+        try {
+            const res = await apiRequest({
+                method: 'POST',
+                url: `/date/update-status`,
+                data: data
+            })
+            setLoader(false)
+            router.push({ pathname: '/user/user-list', query: { city: cityState?.enter_city?.name, country: cityState.enter_country?.value, province: cityState?.enter_city?.province[0]?.short_code?.split("-")[1] ? cityState?.enter_city?.province[0]?.short_code?.split("-")[1] : cityState?.enter_city?.province[0]?.short_code } }, '/user/user-list');
+            dispatch(reset('ChooseCity'))
+            dispatch(reset('CreateStepOne'))
+            dispatch(reset('CreateStepTwo'))
+            dispatch(reset('CreateStepThree'))
+            dispatch(reset('CreateStepFour'))
+        }
+        catch (e) {
+            setLoader(false);
+        }
     }
 
     return (
@@ -69,7 +86,11 @@ const DatePreview = props => {
                                 <Link href="/create-date/choose-city?edit=true"><a>Edit</a></Link>
                             </button>
                             <button type="button" className="next" onClick={postDate}>
-                                <a className="forgot-passwrd">Post Date</a>
+                                {loader ? <span className="spin-loader-button"></span> :
+                                    <>
+                                        <a className="forgot-passwrd">Post Date</a>
+                                    </>
+                                }
                             </button>
                         </div>
                     </div>}
