28d3b38626d631b560af198b9ee0631557039a9e
akkytalk <aakash.prajapati@syspiretechnologies.com>
2022-11-25 22:29:03 +0530

    few changes

--- pages/home.js
+++ pages/home.js
@@ -1,18 +1,14 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 import withAuth from "../core/withAuth";
 
-function Home({ items })  {
+function Home({ items }) {
   return (
     <>
-        <div className="inner-part-page auth-section">
-        
-              <ul>
-               ssdsa
-              </ul>
-        
-        </div>
+      <div className="inner-part-page auth-section">
+        <ul>ssdsa</ul>
+      </div>
     </>
-  )
+  );
 }
-  export default withAuth(Home)
+export default withAuth(Home);
