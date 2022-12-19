cadc98bba087322e0c5daec79439f613e61d8ca4
Akash04-code <akashshukla563@gmail.com>
2022-12-19 12:57:25 +0530

    start home page ui

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,25 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import withAuth from "../core/withAuth";
-
+import LeSlogoWhite from '../assets/LeS logoWhite.png'
 function Home({ items }) {
   return (
     <>
       <div className="inner-part-page auth-section">
-        <ul>ssdsa</ul>
+        <nav class="navbar navbar-dark bg-black">
+          <div class="container-fluid">
+            <a className="LeSociety-Icon-White" href="#">
+              <div className="ls-logo">
+              <Image src={LeSlogoWhite} alt="ls-logo" sizes={10} />
+              </div>
+              <div className="ls-text mt-4">
+               <p> L E S O C I E T Y <br/>
+                   Date outside Your League</p>
+              </div>
+            </a>
+          </div>
+        </nav>
       </div>
     </>
   );
