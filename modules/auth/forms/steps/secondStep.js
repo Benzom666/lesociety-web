d0a808ac124278c51974dc451c0ff74d9d2f4c5f
akkytalk <aakash.prajapati@syspiretechnologies.com>
2023-03-17 00:42:20 +0530

    updated edit content

--- modules/auth/forms/steps/secondStep.js
+++ modules/auth/forms/steps/secondStep.js
@@ -453,8 +453,10 @@ const SecondStep = (props) => {
           </svg>
         </div>
         <div className="text-label">
-          Please continue with your profile <br />
-          to maximize your opportunity
+          {router.query?.edit && router?.query?.type
+            ? "You can only edit what have been requested"
+            : `Please continue with your profile <br />
+        to maximize your opportunity`}
         </div>
         <div className="images-uploads">
           <div className="big-image">
