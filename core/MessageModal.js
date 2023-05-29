5905fdf48303a1af6ae559cdde29d2b2a9b89a17
akkytalk <aakash.prajapati@syspiretechnologies.com>
2023-05-30 00:27:45 +0530

    message modal changes

--- core/MessageModal.js
+++ core/MessageModal.js
@@ -202,7 +202,22 @@ function MessageModal({ user, date, toggle, userMessageNoModal, close }) {
                           border: "none",
                         }}
                       >
-                        <Image
+                        {/* <Image
+                          src={
+                            formProps.values.message === ""
+                              ? "https://secrettime-cdn.s3.eu-west-2.amazonaws.com/secret-time/uploads/message_send.png"
+                              : MessageSend2
+                          }
+                          alt="send-btn"
+                          onClick={() => {
+                            handleSubmit(formProps.values);
+                            formProps.resetForm();
+                          }}
+                          className="no-radius"
+                          width={28}
+                          height={28}
+                        /> */}
+                        <img
                           src={
                             formProps.values.message === ""
                               ? "https://secrettime-cdn.s3.eu-west-2.amazonaws.com/secret-time/uploads/message_send.png"
@@ -305,30 +320,30 @@ function MessageModal({ user, date, toggle, userMessageNoModal, close }) {
                           }}
                           className="message-date-btn"
                         >
-                          {/* <div
+                          <div
                             ref={iconRef}
                             style={{
                               background: "transparent",
                               border: "none",
                             }}
-                          > */}
-                          <Image
-                            src={
-                              formProps.values.message === ""
-                                ? "https://secrettime-cdn.s3.eu-west-2.amazonaws.com/secret-time/uploads/message_send.png"
-                                : MessageSend2
-                            }
-                            alt="send-btn"
-                            type="submit"
-                            onClick={() => {
-                              handleSubmit(formProps.values);
-                              formProps.resetForm();
-                            }}
-                            className="no-radius"
-                            width={28}
-                            height={28}
-                          />
-                          {/* </div> */}
+                          >
+                            <Image
+                              src={
+                                formProps.values.message === ""
+                                  ? "https://secrettime-cdn.s3.eu-west-2.amazonaws.com/secret-time/uploads/message_send.png"
+                                  : MessageSend2
+                              }
+                              alt="send-btn"
+                              type="submit"
+                              onClick={() => {
+                                handleSubmit(formProps.values);
+                                formProps.resetForm();
+                              }}
+                              className="no-radius"
+                              width={28}
+                              height={28}
+                            />
+                          </div>
                         </button>
                       </div>
                     </Form>
