7f69182c36730ec7c961f76432a759f24ff59ba0
akkytalk <aakash.prajapati@syspiretechnologies.com>
2023-06-18 16:03:05 +0530

    changes

--- pages/user/user-list.js
+++ pages/user/user-list.js
@@ -81,6 +81,7 @@ function UserList(props) {
   //   }
   // }, [user]);
 
+  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
   const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
   const isAndroid = /Android/.test(navigator.userAgent);
 
